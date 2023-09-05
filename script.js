acc87d5bd2f0be796995e099c3d552f837082564
gatya <gatya@pipa.hu>
2023-09-05 10:05:02 +0200

    hazikacska

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ console.error('nem')
 document.querySelector('h1').onclick = function () {
   alert('Boldog új évet!')
 }
+
+document.querySelector('h2').onclick = function () {
+  alert('Boldog új évet!')
+}
+
+const kattinthatElem = document.getElementById('kattinthat')
+kattinthatElem.style.cursor = 'pointer'
